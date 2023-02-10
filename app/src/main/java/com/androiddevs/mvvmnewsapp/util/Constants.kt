206192a91a3a4a00aeb86d2d20b107169540a9fd
Blackpaper13 <chris321.yusti@gmail.com>
2023-02-10 23:16:25 +0700

    baru sampai di Retrofit dan Dao. mau ke Database

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "4a26842a083a4d2d9db4030a45ad2776"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
