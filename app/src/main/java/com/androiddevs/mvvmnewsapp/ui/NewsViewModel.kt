d26b383887ad05c374f4b9bee38659b561fad1c4
Blackpaper13 <chris321.yusti@gmail.com>
2023-02-11 05:07:41 +0700

    tinggal internet lost checking

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModel.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModel.kt
@@ -3,6 +3,7 @@ package com.androiddevs.mvvmnewsapp.ui
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.androiddevs.mvvmnewsapp.models.Article
 import com.androiddevs.mvvmnewsapp.models.NewsResponse
 import com.androiddevs.mvvmnewsapp.repository.NewsRepository
 import com.androiddevs.mvvmnewsapp.util.Resource
@@ -14,6 +15,12 @@ class NewsViewModel(
 ) : ViewModel() {
     val breakingNews : MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
     var breakingNewsPage = 1
+    var breakingNewsResponse: NewsResponse? = null
+
+    //inisialisasi untuk page search Result
+    val searchNews : MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var searchNewsPage = 1
+    var searchNewsResponse: NewsResponse? = null
 
     init {
         getBreakingNews("id")
@@ -26,12 +33,55 @@ class NewsViewModel(
 
     }
 
+    fun searchNews(searchQuery: String) = viewModelScope.launch {
+        searchNews.postValue(Resource.Loading())
+        val response = newsRepository.searchNews(searchQuery, searchNewsPage)
+        searchNews.postValue(handleSearchNewsResponse(response))
+    }
+
     private fun handleBreakingNewsResponse(response: Response<NewsResponse>) : Resource<NewsResponse>{
         if (response.isSuccessful) {
             response.body()?.let {
-                return Resource.Success(it)
+                breakingNewsPage++
+                if (breakingNewsResponse == null){
+                    breakingNewsResponse = it
+                } else {
+                    val oldArticle = breakingNewsResponse?.articles
+                    val newArticle = it.articles
+                    oldArticle?.addAll(newArticle)
+                }
+                return Resource.Success(breakingNewsResponse ?: it)
             }
         }
         return Resource.Error(response.message())
     }
+
+    private fun handleSearchNewsResponse(response: Response<NewsResponse>) : Resource<NewsResponse>{
+        if (response.isSuccessful) {
+            response.body()?.let {
+                searchNewsPage++
+                if (searchNewsResponse == null){
+                    searchNewsResponse = it
+                } else {
+                    val oldArticle = searchNewsResponse?.articles
+                    val newArticle = it.articles
+                    oldArticle?.addAll(newArticle)
+                }
+                return Resource.Success(searchNewsResponse ?: it)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+
+    fun saveArticle(article: Article) = viewModelScope.launch {
+        newsRepository.upsert(article)
+    }
+
+    fun getSavedNews() = newsRepository.getSavedNews()
+
+    fun deleteArticle(article: Article) = viewModelScope.launch {
+        newsRepository.deleteArticle(article)
+    }
+
+
 }
